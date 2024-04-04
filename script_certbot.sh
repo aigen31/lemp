94dd0b9edf0964bf2960e1035da08537d51a1b07
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2024-04-05 01:34:30 +0700

    Change certbot script

--- script_certbot.sh
+++ script_certbot.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker run -it --rm --name certbot -v "./letsencrypt/etc:/etc/letsencrypt" -v "./letsencrypt/var:/var/lib/letsencrypt" -p 80:80 certbot/certbot certonly --dry-run
+# Предложение пользователю ввести имя домена
+read -p "Введите имя домена для сертификации: " domain_name
+
+# Запуск сухой попытки создания сертификата
+docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d $domain_name
+
+# Проверка успешности сухой попытки создания сертификата
+read -p "Сертификация выполнена успешно. Хотите продолжить создание сертификата? (y/n): " choice
+
+# Обработка выбора пользователя
+if [ "$choice" = "y" ] || [ "$choice" = "yes" ]; then
+    docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $domain_name
+elif [ "$choice" = "n" ] || [ "$choice" = "no" ]; then
+    echo "Создание сертификата отменено."
+else
+    echo "Неправильный ввод. Пожалуйста, введите 'y/yes' или 'n/no'."
+fi
